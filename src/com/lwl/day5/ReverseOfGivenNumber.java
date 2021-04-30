4cba52e98e0b6f2ab4386a120c75db50a3c1ad68
'avulalakshman' <'learnwithlakshman@gmail.com'>
2021-04-30 08:19:00 +0530

    Written logic to find reverse of the given numbers

--- src/com/lwl/day5/ReverseOfGivenNumber.java
+++ src/com/lwl/day5/ReverseOfGivenNumber.java
@@ -1,5 +1,21 @@
 package com.lwl.day5;
 
+import java.util.Scanner;
+
 public class ReverseOfGivenNumber {
 
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the num:");
+			
+			int num = sc.nextInt();
+			int anum = num;
+			int rev = 0;
+			while(num != 0) {
+				rev = rev * 10 + num %10;
+				num /=10;
+			}
+			System.out.println("Reverse of "+anum+" "+rev);
+			sc.close();
+		}
 }
